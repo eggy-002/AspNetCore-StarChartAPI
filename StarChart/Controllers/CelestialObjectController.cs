ef7cea724889bda1b7ef38dd0f53a2a24dc7deae
Elgardo Echevarria <elgardoe@gmail.com>
2021-03-25 13:01:58 -0400

    module solution

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -12,6 +15,66 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+
+            if (celestialObject == null)
+                return NotFound();
+
+            if (celestialObject.Satellites == null)
+            {
+                celestialObject.Satellites = new List<CelestialObject>();
+            }
+
+            celestialObject.Satellites.Add(celestialObject);
+
+
+            _context.SaveChanges();
+
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(c => c.Name == name).ToList();
+
+            if (celestialObjects.Count == 0)
+                return NotFound();
+
+            foreach(var celestialObject in celestialObjects)
+            {
+                if(celestialObject.Satellites == null)
+                {
+                    celestialObject.Satellites = new List<CelestialObject>();
+                }
+
+                celestialObject.Satellites.Add(celestialObject);
+            }
+
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+
+            foreach (var celestialObject in celestialObjects)
+            {
+                if (celestialObject.Satellites == null)
+                {
+                    celestialObject.Satellites = new List<CelestialObject>();
+                }
+
+                celestialObject.Satellites.Add(celestialObject);
+            }
+
+            return Ok(celestialObjects);
         }
     }
 }
